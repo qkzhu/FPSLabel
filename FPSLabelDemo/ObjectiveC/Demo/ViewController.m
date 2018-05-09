d17b796a2e64ba50021aeff8afe8f0a0b2b35130
QianKun <zqkun.public@gmail.com>
2018-05-10 02:07:22 +0800

    * updated objective-c demo project * added demo gif image

--- FPSLabelDemo/ObjectiveC/Demo/ViewController.m
+++ FPSLabelDemo/ObjectiveC/Demo/ViewController.m
@@ -8,15 +8,20 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSArray *data;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+#pragma mark - life cycle
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self updateData];
+    [self setupTableView];
 }
 
 
@@ -25,5 +30,39 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark - private functions
+- (void)updateData
+{
+    NSMutableArray *array = [NSMutableArray array];
+    for (int i = 0; i < 100; i++)
+    {
+        [array addObject:[NSString stringWithFormat:@"row = %03d", i + 1]];
+    }
+    self.data = array;
+}
+
+- (void)setupTableView
+{
+    self.tableView.dataSource = self;
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.data.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    cell.textLabel.text = [self.data objectAtIndex:indexPath.row];
+    
+    // purposely use a lot cpu in main thread to demo the dropping of FPS.
+    for (NSString *str in self.data) { NSLog(@"%@", str); }
+    
+    return cell;
+}
 
 @end
